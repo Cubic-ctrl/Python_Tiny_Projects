ea5ece518e03a7d127ca395904820969d9f55c6d
Cubic-ctrl <87397092+Cubic-ctrl@users.noreply.github.com>
2023-06-04 14:22:05 +0200

    Update Random+Name+and+Address.py

--- Random+Name+and+Address.py
+++ Random+Name+and+Address.py
@@ -2,6 +2,8 @@
 # importing  faker python module
 from faker import Faker
 
+#creating a function to generate a name randomly and address
+
 fake = Faker()
 name = "Name: " + fake.name()+ "\n"
 address = "Address: " + fake.address()
